511fcc5ba2d3b2d678c9a326b29ea1eff2494f3c
JUHIS6 <94051094+JUHIS6@users.noreply.github.com>
2021-11-10 13:31:30 +0200

    Update Simulator.py

--- Simulator.py
+++ Simulator.py
@@ -4,7 +4,9 @@
 # In[7]:
 
 
-#### This is a simulation file ###
+#### This is a simulation file ####
+#### and meant to be imported  ####
+
 
 import numpy as np
 from scipy.integrate import odeint
@@ -15,7 +17,7 @@ import matplotlib.pyplot as plt
 # In[8]:
 
 
-# Function that returns dx/dt
+### Function that returns dx/dt ###
 def mydiff(t, x, c, k, m, F):
     
     dx1dt = x[1]
@@ -27,37 +29,37 @@ def mydiff(t, x, c, k, m, F):
 # In[9]:
 
 
-### Solve using ODEint, If used swap mydiff t and x args ###
+## Solve using ODEint, If used swap mydiff t and x args ###
 #x = odeint(mydiff, x_init, t)
 #x1 = x[:,0]
 #x2 = x[:,1]
 
 ### Solve using IVP ###
-def generate(c,k,m,F, x0=0):
+def generate(c,k,m,F, x0=0): ## Generate function which can be called to create dataset
 
-    # Initialization
+    ### Initialization ###
     tstart = 0
     tstop = 60
     increment = 1000
 
-    # Initial condition
+    ### Initial condition ###
     x_init = [x0,0]
     #t = np.arange(tstart,tstop+1,increment)
     t= np.linspace(tstart, tstop, increment)
 
-    argss = (c, k, m, F)
+    argss = (c, k, m, F) ## Args for solving
     
     
     x = solve_ivp(mydiff, [tstart, tstop], x_init, args=argss, method='RK45', t_eval=t)
     x1 = x.y[0]
     v1 = x.y[1]
-    return [t, v1, x1]
+    return [t, v1, x1] ## Returning dataset
 
 
 # In[10]:
 
 
-# Plot the Results
+### Plot the Results ###
 
 
 #plt.plot(x.t, x1)
